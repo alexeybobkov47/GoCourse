5ee77f2775942c62828107695b53bf6f97f385e0
Alexeybobkov47 <Alexeybobkov47@gmail.com>
2019-07-01 00:46:14 +0400

    Начало 3 задания.

--- hw5/main.go
+++ hw5/main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"time"
+
+	"io"
+	"os"
 )
 
+type cars struct {
+	year     string `csv:"year"`
+	carbrand string `csv:"carbrand"`
+	carmodel string `csv:"carmodel"`
+	comment  string `csv:"comment"`
+	price    string `csv:"price"`
+}
+
 func main() {
-	start := time.Now()
-	for i := 0; i < 10008934500; i++ {
+	csvFile, err := os.Open("test.csv")
+	if err != nil {
+		return
 	}
-	duration := time.Since(start)
-	fmt.Printf("Operation took: %s", duration)
+	defer csvFile.Close()
+	reader := csv.NewReader(csvFile)
+	for {
+		var car []cars
+
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return
+		}
+		fmt.Println(record)
+
+		car = append(car, cars{
+			year:     record[0],
+			carbrand: record[1],
+			carmodel: record[2],
+			comment:  record[3],
+			price:    record[4],
+		})
+		fmt.Println(car)
+
+	}
 }
