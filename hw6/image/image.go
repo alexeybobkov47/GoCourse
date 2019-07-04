9b8f855e0af4e8f127bcfdd623a968e09de59f77
Бобков Алексей Андреевич <11ap.abobkov@vas.arbitr.ru>
2019-07-04 16:37:39 +0400

    1

--- hw6/image/image.go
+++ hw6/image/image.go
@@ -17,13 +17,13 @@ func main() {
 	// Красим фон в белый.
 	draw.Draw(img, img.Bounds(), &image.Uniform{white}, image.ZP, draw.Src)
 	// Горизонтальные красные полосы
-	for y := 50; y <= 500; y = y + 50 {
+	for y := 0; y <= 500; y = y + 50 {
 		for x := 0; x < 500; x++ {
 			img.Set(x, y, red)
 		}
 	}
 	// Вертикальные синии полосы
-	for x := 50; x <= 500; x = x + 50 {
+	for x := 0; x <= 500; x = x + 50 {
 		for y := 0; y < 500; y++ {
 			img.Set(x, y, blue)
 		}
