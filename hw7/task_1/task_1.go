b93464292aebd3e8a446ae5573f1aae99d2bc11d
Alexeybobkov47 <Alexeybobkov47@gmail.com>
2019-07-11 20:55:27 +0400

    Начало

---
+++ hw7/task_1/task_1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// генерация
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	// возведение в квадрат
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+	// печать
+	for {
+		fmt.Println(<-squares)
+	}
+}
